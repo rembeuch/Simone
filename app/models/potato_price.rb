ba473762d9017c70bc1e2b9af57ca62202dba5c9
rem beuch <beucherie.r@gmail.com>
2024-01-25 19:44:18 +0100

    add model validations

--- app/models/potato_price.rb
+++ app/models/potato_price.rb
@@ -1,2 +1,12 @@
 class PotatoPrice < ApplicationRecord
+    validates :time, presence: true, uniqueness: true
+    validates :value, presence: true, numericality: { greater_than_or_equal_to: 0 }, format: { with: /\A\d+\.\d{2}\z/ }
+    validate :time_cannot_be_in_the_future
+  
+    private
+  
+    def time_cannot_be_in_the_future
+      errors.add(:time, "can't be in the future") if time.present? && time > Time.now
+    end
 end
+  
